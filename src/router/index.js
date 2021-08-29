8331e62a6d489899029a7c49edfe766f15f95236
wilson6508 <wilson650837@gmail.com>
2021-08-29 17:28:59 +0800

    5

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,45 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "@/views/Home.vue";
+import StockHome from "@/components/stock/StockHome";
+import StockInfo from "@/components/stock/StockInfo";
+import StockLog from "@/components/stock/StockLog";
 
 Vue.use(VueRouter);
 
-const routes = [{
-  path: "/",
-  name: "Home",
-  component: Home,
-  meta: {
-    title: "圖表"
-  }
-}, ];
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      title: "首頁"
+    }
+  }, 
+  {
+    path: "/StockHome",
+    name: "StockHome",
+    component: StockHome,
+    children: [
+      {
+        path: "StockInfo",
+        name: "StockInfo",
+        component: StockInfo,
+        meta: {
+          title: "股票資訊"
+        }
+      },
+      {
+        path: "StockLog",
+        name: "StockLog",
+        component: StockLog,
+        meta: {
+          title: "交易紀錄"
+        }
+      },
+    ]
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
