61213bca0c07680a1bb3122083412b8b1e9265ff
wilson6508 <wilson650837@gmail.com>
2022-02-20 17:37:29 +0800

    1

--- src/components/stock/TdTradeLog.vue
+++ src/components/stock/TdTradeLog.vue
@@ -1,58 +1,457 @@
 <template>
   <div>
-    TDTD
-    <b-button @click="demo()">demo</b-button>
-    <b-button @click="update()">update</b-button>
-    <pre>{{ apiResult }}</pre>
+    <div class="row">
+      <div v-show="tradeLog" class="card col m-2">
+        <div class="card-body position-relative">
+          <div class="d-flex align-items-center justify-content-end my-3">
+            <button
+              v-if="rightReadMode"
+              class="btn btn-danger btn-sm mx-1"
+              type="button"
+              @click="rightMode('rightDeleteMode')"
+            >
+              刪除
+            </button>
+            <button
+              v-if="rightReadMode"
+              class="btn btn-success btn-sm"
+              type="button"
+            >
+              新增
+            </button>
+            <button
+              v-if="rightDeleteMode"
+              class="btn btn-primary btn-sm mx-1"
+              type="button"
+            >
+              刪除選取項目
+            </button>
+            <button
+              v-if="rightDeleteMode"
+              class="btn btn-dark btn-sm"
+              type="button"
+              @click="rightMode('rightReadMode')"
+            >
+              取消
+            </button>
+          </div>
+          <div
+            class="table-responsive scrollbar"
+            style="max-height: 300px; overflow-y: auto"
+          >
+            <table class="table mb-0">
+              <!--rightColumns-->
+              <thead class="text-black bg-300">
+                <tr class="text-center">
+                  <th
+                    v-if="rightDeleteMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    <div>
+                      <input class="form-check-input" type="checkbox" />
+                    </div>
+                  </th>
+                  <th
+                    class="align-middle white-space-nowrap"
+                    v-for="(item, index) in rightColumns"
+                    :key="index"
+                  >
+                    {{ item.label }}
+                  </th>
+                  <th
+                    v-if="rightReadMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    編輯
+                  </th>
+                </tr>
+              </thead>
+              <!--data-->
+              <tbody>
+                <tr
+                  v-for="(item, index) in rightApiResult"
+                  :key="index"
+                  class="text-center"
+                >
+                  <td
+                    v-if="rightDeleteMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    <div>
+                      <input class="form-check-input" type="checkbox" />
+                    </div>
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.date }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.symbol }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.ordinaryDividend }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.w8Withholding }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.delta }}
+                  </td>
+                  <td
+                    v-if="rightReadMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    <button
+                      class="btn btn-falcon-default btn-sm"
+                      type="button"
+                      @click="updateDivInfo(item)"
+                    >
+                      編輯
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="card col m-2">
+        <div class="card-body position-relative">
+          <!--按鈕列-->
+          <div class="d-flex align-items-center justify-content-end my-3">
+            <button
+              v-if="rightReadMode"
+              class="btn btn-danger btn-sm mx-1"
+              type="button"
+              @click="rightMode('rightDeleteMode')"
+            >
+              刪除
+            </button>
+            <button
+              v-if="rightReadMode"
+              class="btn btn-secondary btn-sm mx-1"
+              type="button"
+              @click="bigSize()"
+            >
+              放大視窗
+            </button>
+            <button class="btn btn-success btn-sm mx-1" type="button">
+              新增
+            </button>
+            <button
+              v-if="rightDeleteMode"
+              class="btn btn-primary btn-sm mx-1"
+              type="button"
+            >
+              刪除選取項目
+            </button>
+            <button
+              v-if="rightDeleteMode"
+              class="btn btn-dark btn-sm mx-1"
+              type="button"
+              @click="rightMode('rightReadMode')"
+            >
+              取消
+            </button>
+          </div>
+          <!--table-->
+          <div
+            class="table-responsive scrollbar"
+            :style="`max-height: ${mhpx}px; overflow-y: auto`"
+          >
+            <table class="table mb-0">
+              <thead class="">
+                <tr
+                  class="text-center text-black bg-300"
+                  style="position: sticky; top: 0; z-index: 1"
+                >
+                  <th
+                    v-if="rightDeleteMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    <div>
+                      <input class="form-check-input" type="checkbox" />
+                    </div>
+                  </th>
+                  <th
+                    class="align-middle white-space-nowrap"
+                    v-for="(item, index) in rightColumns"
+                    :key="index"
+                  >
+                    {{ item.label }}
+                  </th>
+                  <th
+                    v-if="rightReadMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    編輯
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(item, index) in rightApiResult"
+                  :key="index"
+                  class="text-center"
+                >
+                  <td
+                    v-if="rightDeleteMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    <div>
+                      <input class="form-check-input" type="checkbox" />
+                    </div>
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.date }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.symbol }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.ordinaryDividend }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.w8Withholding }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.delta }}
+                  </td>
+                  <td
+                    v-if="rightReadMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    <button
+                      class="btn btn-falcon-default btn-sm"
+                      type="button"
+                      @click="updateDivInfo(item)"
+                    >
+                      編輯
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-show="highChart" class="row">
+      <div class="card col m-2">
+        <div class="card-body position-relative">
+          <!--新增刪除按鈕-->
+          <div class="d-flex align-items-center justify-content-end my-3">
+            <button
+              v-if="rightReadMode"
+              class="btn btn-danger btn-sm mx-1"
+              type="button"
+              @click="rightMode('rightDeleteMode')"
+            >
+              刪除
+            </button>
+            <button
+              v-if="rightReadMode"
+              class="btn btn-success btn-sm"
+              type="button"
+            >
+              新增
+            </button>
+            <button
+              v-if="rightDeleteMode"
+              class="btn btn-primary btn-sm mx-1"
+              type="button"
+            >
+              刪除選取項目
+            </button>
+            <button
+              v-if="rightDeleteMode"
+              class="btn btn-dark btn-sm"
+              type="button"
+              @click="rightMode('rightReadMode')"
+            >
+              取消
+            </button>
+          </div>
+          <div class="table-responsive scrollbar">
+            <table class="table mb-0">
+              <!--rightColumns-->
+              <thead class="text-black bg-300">
+                <tr class="text-center">
+                  <th
+                    v-if="rightDeleteMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    <div>
+                      <input class="form-check-input" type="checkbox" />
+                    </div>
+                  </th>
+                  <th
+                    class="align-middle white-space-nowrap"
+                    v-for="(item, index) in rightColumns"
+                    :key="index"
+                  >
+                    {{ item.label }}
+                  </th>
+                  <th
+                    v-if="rightReadMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    編輯
+                  </th>
+                </tr>
+              </thead>
+              <!--data-->
+              <tbody class="overflow-auto">
+                <tr
+                  v-for="(item, index) in rightApiResult"
+                  :key="index"
+                  class="text-center"
+                >
+                  <td
+                    v-if="rightDeleteMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    <div>
+                      <input class="form-check-input" type="checkbox" />
+                    </div>
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.date }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.symbol }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.ordinaryDividend }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.w8Withholding }}
+                  </td>
+                  <td class="align-middle white-space-nowrap">
+                    {{ item.delta }}
+                  </td>
+                  <td
+                    v-if="rightReadMode"
+                    class="align-middle white-space-nowrap"
+                  >
+                    <button
+                      class="btn btn-falcon-default btn-sm"
+                      type="button"
+                      @click="updateDivInfo(item)"
+                    >
+                      編輯
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  async mounted() {
+    await this.readDividend();
+  },
   data() {
     return {
-      apiResult: [],
+      rightApiResult: [],
+      rightColumns: [
+        { val: "date", label: "配發日期" },
+        { val: "symbol", label: "股票代號" },
+        { val: "ordinaryDividend", label: "配發股利" },
+        { val: "w8Withholding", label: "w8扣稅" },
+        { val: "delta", label: "股息再投入(股)" },
+      ],
+      rightReadMode: true,
+      rightDeleteMode: false,
+      rightMapping: new Map([
+        [1, "date"],
+        [2, "symbol"],
+        [3, "ordinaryDividend"],
+        [4, "w8Withholding"],
+        [5, "delta"],
+      ]),
+      tradeLog: true,
+      highChart: true,
+      mhpx: 300,
     };
   },
   methods: {
-    async demo() {
+    // 打API
+    async readApi(page) {
       const url = this.getApiUrl("readValue");
       const postBody = {
-        url: this.getApiUrl("secondExcel"),
-        page: 0,
+        url: this.getApiUrl("firstExcel"),
+        page: page,
       };
-      const result = await this.getApiResult(url, postBody);
-      if (result !== null) {
-        this.apiResult = result;
-      }
+      return await this.getApiResult(url, postBody);
     },
-    async update() {
+    async updateApi(page, dataArr) {
       const url = this.getApiUrl("updateValue");
       const postBody = {
-        url: this.getApiUrl("secondExcel"),
-        page: 0,
-        data: [
-          {
-            row: 2,
-            col: 1,
-            val: "NYSEARCA:VOO",
-          },
-          {
-            row: 3,
-            col: 1,
-            val: "NYSEARCA:VOO",
-          },
-          {
-            row: 2,
-            col: 3,
-            val: "2022/2/16",
-          },
-        ],
+        url: this.getApiUrl("firstExcel"),
+        page: page,
+        data: dataArr,
       };
-      const result = await this.getApiResult(url, postBody);
+      await this.getApiResult(url, postBody);
+    },
+    // 讀取table
+    async readDividend() {
+      const result = await this.readApi(7);
       if (result !== null) {
-        console.log(result);
+        this.rightApiResult = result.map((item, index) => {
+          item.rowNum = index + 2;
+          return item;
+        });
       }
+    },
+    // 更新table
+    async updateDividend(rightDataArr) {
+      await this.updateApi(7, rightDataArr);
+    },
+    // 新增資料
+    // 編輯資料
+    async updateDivInfo(item) {
+      const rightDataArr = this.getRightDataArr(item);
+      await this.updateDividend(rightDataArr);
+      await this.readDividend();
+    },
+    getRightDataArr(item) {
+      const dataArr = [];
+      for (let [key, value] of this.rightMapping) {
+        dataArr.push({
+          row: item.rowNum,
+          col: key,
+          val: item[value],
+        });
+      }
+      return dataArr;
+    },
+    // 刪除資料
+    // 控制模式
+    rightMode(str) {
+      this.rightReadMode = false;
+      this.rightDeleteMode = false;
+      switch (str) {
+        case "rightReadMode": {
+          this.rightReadMode = true;
+          break;
+        }
+        case "rightDeleteMode": {
+          this.rightDeleteMode = true;
+          break;
+        }
+      }
+    },
+    // 放大視窗
+    bigSize() {
+      this.highChart = !this.highChart;
+      this.tradeLog = !this.tradeLog;
+      this.mhpx = 700;
     },
   },
 };
